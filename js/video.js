70d491712459373ef69fa784759f0fcb72c4551d
Benspilo <benspilo@umich.edu>
2022-10-31 15:26:57 -0400

    speed done

--- js/video.js
+++ js/video.js
@@ -1,11 +1,34 @@
-var video;
+var video = document.querySelector("#player1");
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
-
+	video.autoplay = false;
+	video.loop = false;
+	// video.defaultPlaybackRate(4)
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+});
 
+document.querySelector("#pause").addEventListener("click", function(){
+	console.log("Pause Video");
+	video.pause();
+
+})
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Video Speed = " + video.playbackRate);
+	video.playbackRate = video.playbackRate* .9;
+});
+
+document.querySelector("#faster").addEventListener("click", function(){
+	console.log("Video Speed = " + video.playbackRate);
+	video.playbackRate = video.playbackRate* 1.1;
+})
+
+document.querySelector("#skip").addEventListener("click", function(){
+	console.log("Video Time = " + video.time);
+	video.time = video.time + 10;
+})
