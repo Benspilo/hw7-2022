ec25343ea47651a5cf230ba45067538648b1c333
Benspilo <benspilo@umich.edu>
2022-11-07 14:01:07 -0500

    mute done

--- js/video.js
+++ js/video.js
@@ -1,6 +1,7 @@
 var video = document.querySelector("#player1");
 
 window.addEventListener("load", function() {
+	var video = document.querySelector("#player1");
 	console.log("Good job opening the window")
 	video.autoplay = false;
 	video.loop = false;
@@ -29,6 +30,30 @@ document.querySelector("#faster").addEventListener("click", function(){
 })
 
 document.querySelector("#skip").addEventListener("click", function(){
-	console.log("Video Time = " + video.time);
-	video.time = video.time + 10;
+	console.log("TimeStamp: " + video.currentTime);
+	video.currentTime = video.currentTime + 10;
+	if (video.currentTime >= video.duration){
+		video.currentTime = 0;
+	}
+})
+
+document.querySelector("#mute").addEventListener("click", function(){
+	if(video.muted == false){
+		console.log("muted");
+		video.muted = true;
+		document.querySelector("#mute").innerHTML = "mute"
+	}
+	else{
+		console.log("unmuted");
+		video.muted = false;
+		document.querySelector("#mute").innerHTML = "unmute"
+
+	}
+})
+
+document.querySelector("#slider").addEventListener("click", function(){
+	console.log("the current volume is " + video.volume);
+	video.volume = this.volume/100;
+
+	innerHtml()
 })
